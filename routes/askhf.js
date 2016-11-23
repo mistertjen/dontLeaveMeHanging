da35fe7d3cd3270b7c1aa4b6924ed548f828157a
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-11-23 18:15:17 +0100

    combined getCurrentPosition API with Google maps API, have to finish still

--- routes/askhf.js
+++ routes/askhf.js
@@ -6,8 +6,43 @@ const router = express.Router()
 let db = require(__dirname + '/../modules/database')
 
 router.route('/askhf')
-	.get((req, res) => {
-		res.render('index')
+	// test if button shows
+	// .get((req, res) => {
+	// 	res.render('index')
+	// })
+	// When submit button for don't leave me hanging is clicked on index
+	.post((req, res) => {
+		// get location
+		let location = "unknown"
+
+		if (!navigator.geolocation){
+			location = "Location is secret";
+			return;
+		}
+
+		function success(position) {
+			let latitude  = position.coords.latitude;
+			let longitude = position.coords.longitude;
+
+		    let locationURL = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=true";
+		    
+		    // Make GET request to locationURL
+		    // Save in variable e.g. unparsedData
+
+		    // Parse the data like this:
+			
+			// let parsedData = JSON.parse(unparsedData)
+			// let city = parsedData.results[0].address_components[3].long_name
+			// let country = parsedData.results[0].address_components[6].long_name
+
+			// location = city + ', ' + country
+		  }
+
+		  function error() {
+		    location = "Location is secret";
+		  }
+
+		  navigator.geolocation.getCurrentPosition(success, error);
 	})
 
 // module.exports says: the current file when required will send back this thing
