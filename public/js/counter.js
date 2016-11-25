6c7a947baee714f9598957f9408e45b47c51087e
mistertjen <mistertjen@hotmail.com>
2016-11-25 13:58:44 +0100

    finished. Cleaned up code, seperate file func.js for all re-usable functions.

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,46 +1,16 @@
-const getHFTimeMs = (route, callback) => {
-	$.get(route, (success) => {
-		const result = Date.parse(success)
-		if (typeof callback == 'function') callback(result)
-	})
-}
-
-// const msToTime = (ms) => {
-// 	// transform to seconds
-// 	x = ms / 1000;
-// 	// get amount of seconds that are not a whole minute
-// 	seconds = Math.floor(x % 60)
-// 	// transform to minutes
-// 	x /= 60
-// 	// get amount of minutes that are not a whole hour
-// 	minutes = Math.floor(x % 60)
-// 	// transform to hours
-// 	x /= 60
-// 	// get amount of hours that are not a whole day
-// 	hours = Math.floor(x % 24)
-// 	// transform to days
-// 	days = Math.floor(x / 24)
-// 	const result = {seconds: seconds, minutes: minutes, hours:hours, days: days}
-// 	return result
-// }
-
-// minified
-const msToTime = (ms) => {
-	x = ms / 1000;seconds = Math.floor(x % 60);x /= 60;minutes = Math.floor(x % 60);x /= 60;hours = Math.floor(x % 24);days = Math.floor(x / 24);const result = {seconds: seconds, minutes: minutes, hours:hours, days: days};return result
-}
-
 // function only called on pages that include a timer class and adds the timeString to the html element.
 if ($('.timer').length > 0) { 
 	// store dom object, to prevent constant jquery calls.
 	const target = $('.timer')
 
-	getHFTimeMs('/timer/HFAskTime', (HFTime) => {
-		const now = Date.now()
-		let difference = now - HFTime
+	func.getHFTimeMs('/timer/HFAskTime', (HFTime) => {
+		const now = Date.now() // in ms
+		let difference = now - HFTime // in ms
 
 		// save action to be done in interval
 		const action = () => {
-			let timeObj = msToTime(difference)
+			// save the resulting obj from msToTime
+			let timeObj = func.msToTime(difference)
 			// uses es6 template string
 			let timeString = `You have been left having for: ${timeObj.days} days, ${timeObj.hours} hours, ${timeObj.minutes} minutes, ${timeObj.seconds} seconds. `
 			target.html(timeString)
