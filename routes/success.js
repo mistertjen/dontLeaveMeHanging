c12c8df1aba2b532bcf4fca2cf43d3895d37ebcd
mistertjen <mistertjen@hotmail.com>
2016-11-25 17:45:07 +0100

    does not work yet.

--- routes/success.js
+++ routes/success.js
@@ -7,16 +7,45 @@ let db = require(__dirname + '/../modules/database')
 
 router.route('/success')
 	.get((req, res) => {
+		// db.User.findById(req.session.user.id)
+		// .then
+		// 	where: {
+		// 		id:
+		// 	}
+		// })
 		db.HFAsk.findOne({
 			where: {
-				hfgiveId: true
+				hfgiveId: {
+					$ne: null
+				}
+				// $not:
+				// 	{id:2}
+				// }	
+				// }
+				// $and: {
+				// 	id: {
+				// 		$not: 1
+				// 	}
+				// }
+				// hfgiveId: true
+				// id: {
+				// 	$ne: 1
+				// }
+				// $notIn: [{
+				// 	id: null
+				// }]
 			}
 		})
 		.then(x => {
-			res.send('success!!!')
+			if (x == null) res.send('failed')
+			else res.send('success!!!')
+			console.log(x.dataValues)
+			// res.end()
+			// console.log(x)
 		})
 		.catch(x => {
 			res.send('')
+			console.log(x)
 		})
 	})
 
