7e9ded257e90107b572abf16736e7839fe0a1ed8
mistertjen <mistertjen@hotmail.com>
2016-11-28 09:51:03 +0100

    feature works. Need to discuss with team how to best structure the code.

--- public/js/success-script.js
+++ public/js/success-script.js
@@ -1,7 +1,12 @@
 const checkSuccess = () => {
-	$.get('/success', (data) => {
+	// post to route, data gets true or false
+	$.post('/success', (data) => {
 		console.log(data)
+		// if true (HF is matched in database), then go to /success route
+		if(data) location.href = "/success"
 	})
 }
 
+// initial check and once again every 2 sec (2000ms).
 checkSuccess()
+setInterval(checkSuccess, 2000)
