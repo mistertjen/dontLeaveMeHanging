5bc4e8742499e90e3c9458c39989203145d2ca2e
mistertjen <mistertjen@hotmail.com>
2016-11-28 17:52:32 +0100

    small fixes and comments

--- public/js/success-lib.js
+++ public/js/success-lib.js
@@ -1,8 +1,7 @@
 const successFunc = {
 	check: () => {
-		// post to route, data gets true or false
+		// post to standard route, data gets true or false
 		$.get('/success/check', (data) => {
-			console.log(data)
 			// if true (HF is matched in database), then go to /success route
 			if(data) location.href = "/success"
 		})
