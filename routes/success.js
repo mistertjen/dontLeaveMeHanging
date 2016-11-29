1fb48344fc62fa6c4fc7ce72ecbb1e9484e2f736
mistertjen <mistertjen@hotmail.com>
2016-11-29 12:50:12 +0100

    success works again. Other features dont break

--- routes/success.js
+++ routes/success.js
@@ -7,7 +7,64 @@ let db = require(__dirname + '/../modules/database')
 
 router.route('/success')
 	.get((req, res) => {
-		res.send('route works')
+		// only render if a matchPath is set.
+		if (req.session.matchedPath) {
+			res.render('success', {matchedPath: req.session.matchedPath})
+		}
+		// else back to root.
+		else {res.redirect('/')}
+	})
+
+router.route('/success/check')
+	.get((req, res) => {
+		db.HFAsk.findOne({
+			where: {
+				// PRODUCTION:
+				userId: req.session.user.id,
+				// TEST:
+				// userId: 1,
+				hfgiveId: {
+					$ne: null
+				}
+			},
+			include: {all:true}
+		})
+		.then(HFAsk => {
+			if (HFAsk == null) res.send(false)
+			else {
+				// if not null, then store the matched HF and Path in the session data.
+				req.session.matchedPath = 'HFAsk'
+				req.session.matchedHF = HFAsk
+				//  send true to the client side (the AJAX req), then redirect from there. Res.render not possible here.
+				res.send(true)
+			}
+		})
+		.catch(err => {
+			res.send(false)
+			console.log(err)
+		})
+	})
+
+router.route('/success/matchedHF')
+	.get((req, res) => {
+		// find the username of the matching hf. 
+		db.User.findById(req.session.matchedHF.hfgive.userId)
+		.then (HFGiveUser => {
+			console.log(req.session.matchedHF)
+			// create object with only the specific data to send back.
+			let data = {
+				HFAskTime: req.session.matchedHF.updatedAt,
+				HFGiveTime: req.session.matchedHF.hfgive.updatedAt,
+				HFGiveLocation: req.session.matchedHF.hfgive.location,
+				HFGiveUserName: HFGiveUser.name
+			}
+			res.send(data)
+			// clear session data of the match
+			db.HFAsk.findById(req.session.matchedHF.id).then(match => match.destroy())
+
+			req.session.matchedHF = ''
+			req.session.matchedPath = ''
+		})
 	})
 
 // module.exports says: the current file when required will send back this thing
